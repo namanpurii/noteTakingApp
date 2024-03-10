688998a61d0a657f5167bf4822b1db7b1218ff70
namanpurii <puri2001naman@gmail.com>
2024-03-10 23:34:46 +0530

    drawing on canvas enabled. **bug: cant redraw on reload**

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
-import React from "react"
+import React, { useState } from "react"
 import { useEffect } from "react"
 
 function App() {
-  useEffect(()=>{
-    const canvas = document.getElementById("main-canvas") as HTMLCanvasElement
-    if(canvas){
-      const ctx = canvas.getContext("2d") 
-      if(ctx) {
-        ctx.fillStyle = "#121212"
-        ctx.fillRect(0,0, window.innerWidth, window.innerHeight)
-        ctx.fillStyle = "#BB86FC"
-        ctx.font = "30px Arial";
-        ctx.fillText("Note Taking App", 10, 50);
-      }
+  let isDown = false
+  let canvas: HTMLCanvasElement|null, ctx:CanvasRenderingContext2D|null, canvasX, canvasY
+  canvas = document.getElementById("main-canvas") as HTMLCanvasElement
+  if(canvas){
+    ctx = canvas.getContext("2d") 
+    if(ctx) {
+      ctx.fillStyle = "#FFFFFF"
+      ctx.lineWidth = 5
+      ctx.fillRect(0,0, window.innerWidth, window.innerHeight)
     }
-  }, [])
+  }
+
+  function handleMouseDown(e:MouseEvent) {
+    isDown = true
+    if(canvas && ctx) {
+        canvasX = e.pageX
+        canvasY = e.pageY
+        ctx.beginPath()
+        ctx.moveTo(canvasX, canvasY)
+    }
+  }
+
+  function handleMouseMove(e:MouseEvent) {
+    if(isDown && canvas && ctx) {
+      canvasX = e.pageX 
+      canvasY = e.pageY
+      ctx.lineTo(canvasX, canvasY)
+      ctx.strokeStyle = "#000000"
+      ctx.stroke()
+    }
+  }
+  function handleMouseUp(e:MouseEvent) {
+    isDown = false
+    ctx?.save()
+  }
+
   return (
     <>
       <div className="overflow-hidden">
-        <h1 className="text-xl">Work in Progress ...</h1>
-        <canvas id="main-canvas" width={window.innerWidth} height={window.innerHeight}>this is the main canvas</canvas>
+        <canvas id="main-canvas" width={window.innerWidth} height={window.innerHeight} onMouseDown={handleMouseDown} onMouseMove={handleMouseMove} onMouseUp={handleMouseUp}>Canvas is not supported by your broswer</canvas>
       </div>
     </>
   )
